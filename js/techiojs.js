1d7170ac42434f28cd8301c083c99d44feaf0d6a
Léon <leon.vandebeek@xs4all.nl>
2017-12-06 22:32:08 +0100

    All content + finished how the cards look

--- js/techiojs.js
+++ js/techiojs.js
@@ -1,14 +1,10 @@
 $(document).ready(() => {
-  $('.infobutton').on('click', () => {
-    $('.info').slideToggle();
 
-  })
-  $('.addtocartbutton').on('click', () => {
-    BootstrapDialog.show({
-            message: 'Hi Apple!'
-        });
-
-  })
-
-
+  $('.container2').on('mouseenter', event => {
+    $(event.currentTarget).children().siblings('.info').fadeIn(150);
+    $(event.currentTarget).children('.fadeimage').fadeTo(400, 0.3);
+  }).on('mouseleave', event => {
+    $(event.currentTarget).children(".fadeimage").fadeTo(400, 1);
+    $(event.currentTarget).children().siblings('.info').fadeOut(150);
+  });
 });
